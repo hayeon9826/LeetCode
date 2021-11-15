be310c1249e113d8b3b2689ef1483a625b1c04b2
insomenia-hayeon <khy@insomenia.com>
2021-11-15 23:53:52 +0900

    edit comment

--- 14 Days Study Plan to Crack Algo/Two Pointers/189_rotate_array.py
+++ 14 Days Study Plan to Crack Algo/Two Pointers/189_rotate_array.py
@@ -2,7 +2,13 @@
 # Other answer:
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         # x % y (remainder of x/y)
+        # problem if k is larger than len(nums), thus calculate the remainder of k % len(nums)
         k = k % len(nums)
         # [:] is the array slice syntax for every element in the array.
         nums[:] = nums[-k:] + nums[:-k]
+
+        # same as nums[:] = nums[length - k : length] + nums[:length - k]
