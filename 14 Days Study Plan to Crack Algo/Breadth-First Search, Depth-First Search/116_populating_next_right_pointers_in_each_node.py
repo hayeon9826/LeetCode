8885f2c4c7db51df2a294168797076cd90bd7532
insomenia-hayeon <khy@insomenia.com>
2021-11-22 23:57:44 +0900

    bfs, dfs

--- 14 Days Study Plan to Crack Algo/Breadth-First Search, Depth-First Search/116_populating_next_right_pointers_in_each_node.py
+++ 14 Days Study Plan to Crack Algo/Breadth-First Search, Depth-First Search/116_populating_next_right_pointers_in_each_node.py
@@ -61,3 +61,25 @@ def connect(self, root):
                 curr.right.next = curr.next.left
             stack.append(curr.right)
             stack.append(curr.left)
+
+
+  class Solution(object):
+    def connect(self, root):
+        """
+        :type root: TreeLinkNode
+        :rtype: nothing
+        """
+        
+        if not root:
+            return None
+        cur  = root
+        next = root.left
+
+        while cur.left :
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = next
+                next = cur.left
