a289309bd04524f5423690c0af04c9411488389d
hayeon9826 <hayeon226@likelion.org>
2021-12-25 23:15:36 +0900

    add example

--- 14 Days Study Plan to Crack Algo/Recursion, Backtracking/784_letter_case_permutation.py
+++ 14 Days Study Plan to Crack Algo/Recursion, Backtracking/784_letter_case_permutation.py
@@ -41,3 +41,21 @@ class Solution(object):
         res = []
         backtrack()
         return res
+
+# https://leetcode.com/problems/letter-case-permutation/discuss/369127/Python3-backtracking-solution
+class Solution:
+	def letterCasePermutation(self, S: str) -> List[str]:
+		self.res=[]
+		self.dfs(S,'',0)
+		return self.res
+
+	def dfs(self,S,path,index):
+		if index==len(S):
+			self.res.append(path)            
+			return
+
+		if S[index].isalpha():
+			self.dfs(S,path+S[index].lower(),index+1)
+			self.dfs(S,path+S[index].upper(),index+1)
+		else:
+			self.dfs(S,path+S[index],index+1)
