3fcf46b8f77f5ac3301b950e3abf4bb0be9c03ba
hayeon9826 <hayeon226@likelion.org>
2022-01-05 23:58:03 +0900

    array stock

--- Data Structure/Array/121_best_time_to_buy_and_sell_stock.py
+++ Data Structure/Array/121_best_time_to_buy_and_sell_stock.py
@@ -1,3 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
         
+        for i in range(0, len(prices)-2):
+            for j in range(1,len(prices)-1):
+                maxProfit = max((prices[j] - prices[i]), maxProfit)
+                
+        return maxProfit if maxProfit > 0 else 0
