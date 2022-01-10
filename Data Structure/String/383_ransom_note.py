10462db6e3013b1aabba62b8981adae308ed03fe
hayeon9826 <hayeon226@likelion.org>
2022-01-11 00:05:59 +0900

    string ransomnote

--- Data Structure/String/383_ransom_note.py
+++ Data Structure/String/383_ransom_note.py
@@ -1,3 +1,23 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = "".join(sorted(ransomNote))
+        magazine = "".join(sorted(magazine))
         
+        for char in ransomNote:
+            if char in magazine:
+                magazine = magazine.replace(char, "", 1)
+            else:
+                return False
+        return True
+
+
+# Faster
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for char in set(ransomNote):
+            if magazine.count(char) < ransomNote.count(char):
+                return False
+        return True
+        
+# Runtime: 36 ms, faster than 95.60% of Python3 online submissions for Ransom Note.
+# Memory Usage: 14.5 MB, less than 9.62% of Python3 online submissions for Ransom Note.
