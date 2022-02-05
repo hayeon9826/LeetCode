f765ac21ce1754716a86d3a4e465826381ab0fbd
hayeon9826 <hayeon226@likelion.org>
2022-02-05 23:46:58 +0900

    hashmap algo

--- Data Structure 2/Array/706_design_hashmap.py
+++ Data Structure 2/Array/706_design_hashmap.py
@@ -1,15 +1,20 @@
 class MyHashMap:
-
+    
     def __init__(self):
+        self.hashMap = {}
         
-
     def put(self, key: int, value: int) -> None:
-        
+        self.hashMap[key] = value
 
     def get(self, key: int) -> int:
+        if key in self.hashMap:
+            return self.hashMap[key]
+        else:
+            return -1
         
-
     def remove(self, key: int) -> None:
+        if key in self.hashMap:
+            del self.hashMap[key]
         
 
 
@@ -18,7 +23,6 @@ class MyHashMap:
 # obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
-
 
 
 # source: https://leetcode.com/problems/design-hashmap/discuss/1732515/python
