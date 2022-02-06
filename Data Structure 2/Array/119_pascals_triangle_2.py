0508fd653ddef6d964b2edadc417ea71571a2f0d
hayeon9826 <hayeon226@likelion.org>
2022-02-06 23:54:47 +0900

    pascal triangle answers

--- Data Structure 2/Array/119_pascals_triangle_2.py
+++ Data Structure 2/Array/119_pascals_triangle_2.py
@@ -17,3 +17,27 @@ class Solution:
             rowIndex = rowIndex - 1
             
         return answer
+
+# for 문 만을 이용한 반복문:
+# source: https://leetcode.com/problems/pascals-triangle-ii/discuss/1742190/Python-3-(40ms)-or-Iterative-Easy-Approach
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res=[]
+        for i in range(rowIndex+1):
+            res.append([])
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    res[i].append(1)
+                else:
+                    res[i].append(res[i - 1][j - 1] + res[i - 1][j])
+        return res[rowIndex]
+
+
+# faster code
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        answer=[1]*(rowIndex+1)
+        for i in range(2, rowIndex+1):
+            for j in range(1, i):
+                answer[i-j]+=answer[i-j-1]
+        return answer
