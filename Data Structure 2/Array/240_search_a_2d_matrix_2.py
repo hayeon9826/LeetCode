81822d1081176cbdc667887da5eac1087fac067e
hayeon9826 <hayeon226@likelion.org>
2022-02-10 22:15:13 +0900

    search 2d matrix

--- Data Structure 2/Array/240_search_a_2d_matrix_2.py
+++ Data Structure 2/Array/240_search_a_2d_matrix_2.py
@@ -7,3 +7,27 @@ class Solution:
                 if matrix[i][j] == target:
                     return True
         return False
+
+# with binary search (Better)
+# source: https://leetcode.com/problems/search-a-2d-matrix-ii/discuss/528263/Two-efficient-python-sol.-sharing.-90%2B-w-Diagram
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        h, w = len(matrix), len(matrix[0])
+        
+        for row in matrix:
+			# range check
+            if row[0] <= target <= row[-1]:
+				# launch binary search on current possible row
+                left, right = 0, w-1
+                while left <= right:
+                    mid = left + (right - left) // 2
+                    mid_value = row[mid]
+                    
+                    if target > mid_value:
+                        left = mid+1
+                    elif target < mid_value:
+                        right = mid-1
+                    else:
+                        return True
+        return False
