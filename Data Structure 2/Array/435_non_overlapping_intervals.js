92577abb47bc203bc5cc5e240d35c025d4a7c0c9
hayeon9826 <hayeon226@likelion.org>
2022-02-12 23:56:23 +0900

    add non overlapping intervals answer

--- Data Structure 2/Array/435_non_overlapping_intervals.js
+++ Data Structure 2/Array/435_non_overlapping_intervals.js
@@ -2,4 +2,24 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-var eraseOverlapIntervals = function (intervals) {};
+var eraseOverlapIntervals = function (intervals) {
+  if (intervals.length == 1) {
+    return 0;
+  }
+
+  intervals.sort();
+  let currentEnd = intervals[0][1];
+  let count = 0;
+
+  for (let i = 1; i < intervals.length; i++) {
+    console.log(intervals[i]);
+    if (intervals[i][0] < currentEnd) {
+      count += 1;
+      currentEnd = Math.min(currentEnd, intervals[i][1]);
+    } else {
+      currentEnd = intervals[i][1];
+    }
+  }
+
+  return count;
+};
