0c616d9efc8632fd9f3d384223b686fc2220d38e
hayeon9826 <hayeon226@likelion.org>
2022-02-17 20:19:19 +0900

    add product of array

--- Data Structure 2/Array/238_product_of_array_except_self.js
+++ Data Structure 2/Array/238_product_of_array_except_self.js
@@ -2,4 +2,32 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var productExceptSelf = function (nums) {};
+var productExceptSelf = function (nums) {
+  let answer = [];
+  for (i = 0; i < nums.length; i++) {
+    let value = nums.reduce(
+      (total, val, idx) => (idx == i ? total : total * val),
+      1
+    );
+    answer.push(value);
+  }
+
+  return answer;
+};
+
+var productExceptSelf = function (nums) {
+  const result = [];
+  let productSoFar = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = productSoFar;
+    productSoFar *= nums[i];
+  }
+
+  productSoFar = 1;
+  for (let j = nums.length - 1; j >= 0; j--) {
+    result[j] *= productSoFar;
+    productSoFar *= nums[j];
+  }
+
+  return result;
+};
