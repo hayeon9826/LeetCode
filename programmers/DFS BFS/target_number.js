bce9efe7d4e03132a8735e84b6c8d582060ebd8c
hayeon9826 <hayeon226@likelion.org>
2022-03-10 23:49:51 +0900

    dfs/bfs programmers

---
+++ programmers/DFS BFS/target_number.js
@@ -0,0 +1,19 @@
+function solution(numbers, target) {
+  let answer = 0;
+
+  function dfs(depth, sum) {
+    if (depth === numbers.length) {
+      if (sum === target) {
+        answer += 1;
+      }
+      return;
+    }
+
+    dfs(depth + 1, sum + numbers[depth]); // dfs tree 왼쪽
+    dfs(depth + 1, sum - numbers[depth]); // dfs tree 오른쪽
+  }
+
+  dfs(0, 0);
+
+  return answer;
+}
