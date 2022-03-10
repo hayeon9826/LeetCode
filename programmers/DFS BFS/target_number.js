4edd92f9e964355f0479267318cd94236c138692
hayeon9826 <hayeon226@likelion.org>
2022-03-11 00:50:17 +0900

    dfs/bfs programmers add comment

--- programmers/DFS BFS/target_number.js
+++ programmers/DFS BFS/target_number.js
@@ -1,6 +1,6 @@
 function solution(numbers, target) {
   let answer = 0;
-
+  // dfs 정의. 만약 depth(총 갯수)를 끝까지 내려갔을 때 값이 target과 같으면 +1
   function dfs(depth, sum) {
     if (depth === numbers.length) {
       if (sum === target) {
@@ -9,10 +9,12 @@ function solution(numbers, target) {
       return;
     }
 
+    // 재귀
     dfs(depth + 1, sum + numbers[depth]); // dfs tree 왼쪽
     dfs(depth + 1, sum - numbers[depth]); // dfs tree 오른쪽
   }
 
+  // dfs 시작
   dfs(0, 0);
 
   return answer;
